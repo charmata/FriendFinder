088ebc220be4f8ebfa909386e4dc22499c411cfd
Chris Armata <chris.armata@gmail.com>
2019-05-06 23:56:47 -0400

    Setup friends storage

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    name: "Bane",
+    photo: "https://vignette.wikia.nocookie.net/batman/images/f/f0/Bane_TDKR3.jpg/revision/latest?cb=20120511112335",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,13 @@
 var path = require("path");
+var friends = require("../data/friends");
 
 module.exports = app => {
   app.get("/api/friends", (req, res) => {
-    res.json();
+    res.json(friends);
   });
 
   app.post("/api/friends", (req, res) => {
+    friends.push({ name: req.body.name, photo: req.body.photo, scores: req.body.scores });
     res.send("Successfully added friend");
   });
 };
